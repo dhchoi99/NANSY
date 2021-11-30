ba62ec7fdcd5c83d6b32b9bbc02b1e6215be575f
dongho.choi <dongho.choi@snu.ac.kr>
2021-11-30 15:26:52 +0900

    add codes for css10 dataset with speaker_id support

--- datasets/css10.py
+++ datasets/css10.py
@@ -44,6 +44,48 @@ class CSS10Dataset(CustomDataset):
         return data_list
 
 
+class CSS10AllDataset(CustomDataset):
+    def __init__(self, conf):
+        super(CSS10AllDataset, self).__init__(conf)
+
+        self.data = self.read_metadata()
+
+    def read_metadata(self):
+        subdatasets = self.conf.subdatasets
+
+        data_list = []
+        for idx, language in enumerate(subdatasets.keys()):
+            subdataset = subdatasets[language]
+            with open(subdataset['metadata'], 'r') as f:
+                file_lists = f.readlines()
+
+            train_val_split_idx = int(len(file_lists) * 0.9)
+            if self.conf.mode == 'train':
+                file_lists = file_lists[:train_val_split_idx]
+            elif self.conf.mode == 'eval':
+                file_lists = file_lists[train_val_split_idx:]
+            elif self.conf.mode == 'all':
+                pass
+            else:
+                raise NotImplementedError
+
+            for jdx, line in enumerate(file_lists):
+                line = line.strip()
+                wav_path, original_script, normalized_script, audio_duration = line.strip().split('|')
+                speaker_id = wav_path.split('/')[0]
+                wav_path_22k = os.path.join(subdataset['root'], wav_path)
+                data = {
+                    'wav_path_22k': wav_path_22k,
+                    'wav_path_16k': None,
+                    'text': normalized_script,
+                    'speaker_id': speaker_id,
+                    'language': language,
+                }
+                data_list.append(data)
+
+        return data_list
+
+
 if __name__ == '__main__':
     from omegaconf import OmegaConf
 
