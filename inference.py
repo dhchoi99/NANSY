65fe76fadc08b236316b4ef52953cda078a320d6
dongho.choi <dongho.choi@snu.ac.kr>
2021-11-30 16:41:12 +0900

    update inference code, now tsa works

--- inference.py
+++ inference.py
@@ -6,6 +6,7 @@ import numpy as np
 from omegaconf import OmegaConf
 import torch
 import torchaudio.functional as AF
+from tqdm import tqdm, trange
 
 from datasets.base import MultiDataset
 from trainer import Trainer
@@ -14,7 +15,7 @@ from trainer import Trainer
 class TSAHelper(torch.nn.Module):
     def __init__(self):
         super(TSAHelper, self).__init__()
-        self.embedding = torch.nn.Parameter(torch.zeros(1, 1024, 73).float())
+        self.embedding = torch.nn.Parameter(torch.zeros(1, 1024, 74).float())
 
     def forward(self, x):
         y = x + self.embedding
@@ -22,23 +23,25 @@ class TSAHelper(torch.nn.Module):
 
 
 def main():
-    path_conf = 'configs/train_nansy.yaml'
+    path_conf = '/root/NANSY/configs/train_nansy.yaml'
+
+    path_ckpt = '/raid/vision/dhchoi/log/nansy/29/checkpoints/epoch=1-step=9099.ckpt'
+
+    path_audio_source = '/raid/vision/dhchoi/data/VCTK-Corpus/wav22/p376/p376_290-22k.wav'
+    path_audio_target = '/raid/vision/dhchoi/temp/DS2632_00322.wav'
     conf = OmegaConf.load(path_conf)
 
     model = Trainer(conf)  # TODO get trainer from conf
     model.eval()
 
-    path_ckpt = '/raid/vision/dhchoi/log/nansy/16/checkpoints/epoch=47-step=52367.ckpt'
     data_ckpt = torch.load(path_ckpt, map_location='cpu')
 
     model.load_state_dict(data_ckpt['state_dict'])
-
-    path_audio_source = '/raid/vision/dhchoi/data/VCTK-Corpus/wav22/p376/p376_290-22k.wav'
-    path_audio_target = '/raid/vision/dhchoi/temp/DS2632_00322.wav'
+    model.cuda(1)
 
     d = MultiDataset(conf.datasets.train)
-    tsa_helper = TSAHelper()
 
+    tsa_helper = TSAHelper().cuda(1)
     opt = torch.optim.Adam(tsa_helper.parameters(), lr=1e-4, betas=(0.5, 0.9))
 
     _, wav_22k_source = d.datasets['0'].load_wav(path_audio_source, 22050)
@@ -47,6 +50,7 @@ def main():
     mel_22k = d.datasets['0'].load_mel(path_audio_source, sr=22050)
 
     self = d.datasets['0']
+
     return_data = {}
     # for idx in range(0, mel_22k.shape[1-1].shape[-1]-1, self.mel_len):
 
@@ -58,7 +62,9 @@ def main():
         'gen_source_tsa': [],
         'gen_target_tsa': [],
     }
-    for idx in range(0, mel_22k.length + d.datasets['0'].mel_window, d.datasets['0'].mel_window):
+
+    for idx in range(0, mel_22k.shape[-1], self.mel_window):
+        print(idx)
         mel_start = idx
         mel_end = mel_start + self.mel_window
         gt_mel_22k = self.crop_audio(mel_22k, mel_start, mel_end, -4)
@@ -84,9 +90,10 @@ def main():
         return_data['source_22k_mel'] = gt_mel_22k
 
         #     return_data['t'] = t_start
-        batch = {key: value.unsqueeze(0) for key, value in return_data.items()}
+        batch = {key: value.unsqueeze(0).cuda(1) for key, value in return_data.items()}
 
         audios['gt_source'].append(batch['source_22k'][0].cpu().numpy())
+        audios['gt_target'].append(batch['target_16k'][0].cpu().numpy())
 
         with torch.no_grad():
             lps = model.networks['Analysis'].linguistic(batch['source_16k'])
@@ -104,12 +111,13 @@ def main():
             result = model.networks['Synthesis'](lps, s_tgt, e, ps)
             audios['gen_target'].append(result['audio_gen'][0].cpu().numpy())
 
-        for i in range(100):  # test time self adaptation
-            lps = tsa_helper(lps)
-            result = model.networks['Synthesis'](lps, s_src, e, ps)
+        for _ in trange(100):  # test time self adaptation
+            lps_tsa = lps.detach().clone()
+            lps_tsa = tsa_helper(lps_tsa)
+            result = model.networks['Synthesis'](lps_tsa, s_src, e, ps)
 
             opt.zero_grad()
-            loss_mel = torch.nn.functional.l1_loss(batch['source_22k_mel'], result['gt_mel_22k'])
+            loss_mel = torch.nn.functional.l1_loss(batch['source_22k_mel'], result['gen_mel'])
             loss_mel.backward()
             opt.step()
 
@@ -122,7 +130,11 @@ def main():
 
     final_audios = {}
     for key, value in audios.items():
-        final_audios[key] = np.stack(value, axis=0)
+        try:
+            final_audios[key] = np.concatenate(value, axis=-1)
+            print(key, len(value), final_audios[key].shape)
+        except Exception as e:
+            print(key, e)
 
 
 if __name__ == '__main__':
